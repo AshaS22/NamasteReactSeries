eb2da1d6e923cf2e1188a8999ccf5177fe21ab50
Asha S <AshaS@Chethans-MacBook-Pro.local>
2023-12-27 12:56:36 +0530

    Episode 2 & 3

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import {HeaderAssignment} from './HeaderAssignment';
 
-// create h1 in react
-//Object is where we give attribute to tags
-const heading = React.createElement('h1', {id:'heading'}, "Namaste from react")
-// We need to tell react which is the root
-const elem = ReactDOM.createRoot(document.getElementById('root'))
-elem.render(heading);
+//React.createElement => Creates an Object => When we render this Object on DOM, it becomes an HTML Element
 
-// React element is nothing but a normal JS Object. Prints an object of type React.Element
-console.log(heading)
+// const heading = React.createElement("h1", {}, "Namaste from Reacttttt");
 
-//Props are children+attributes that we pass in ̰ß
+//JSX
+
+// React Element
+// JSX ------>(Transpiled)ReactElement => JS Object => HTML ELement
+const h1 = <h1>Namaste from React1</h1>;
+
+// React functional component
+const Heading = () => <h1>React functional component</h1>;
+
+// Assignment
+const header = (
+  <div className="title" id="1" tabIndex={0}>
+    <h1>
+      <h2>
+        <h3>Hello from element</h3>
+      </h2>
+    </h1>
+  </div>
+);
+
+// Component composition
+const Header = () => (
+  <div className="title">
+    <h1>
+      <h2>
+        <h3>Hello from Component</h3>
+      </h2>
+    </h1>
+    {header}
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderAssignment />);
